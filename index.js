6d65c6aa575661e55d3cdb236eaf64f556ff5a03
Emi <30377472+Emi2005e@users.noreply.github.com>
2026-04-20 23:11:05 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 const express= require("express");
+const pg= require("pg");
 const path= require("path");
 const fs=require("fs");
 const sass=require("sass");
+const sharp = require('sharp');
+
+client=new pg.Client({
+    database:"cti_2026",
+    user:"emi",
+    password:"emi",
+    host:"localhost",
+    port:5432
+})
+
+client.connect()
+
+client.query("select * from prajituri where id>3", function(err, rez){
+    if (err){
+        console.log("Eroare", err)
+    }
+    else{
+        console.log(rez)
+    }
+})
 
 
 // Bonus etapa 4
@@ -106,7 +127,7 @@ pe cand process.cwd() returneaza folderul din care ai deschis terminalul si ai d
 */
 
 
-//Cerinta 20 etapa 4
+//Cerinta 20 etapa 4 si etapa 5 cerinta 2 a
 let vect_foldere = ["temp", "logs", "backup", "fisiere_uploadate"];
 for (let folder of vect_foldere) {
     let caleFolder = path.join(__dirname, folder);
@@ -119,11 +140,10 @@ for (let folder of vect_foldere) {
 app= express();
 app.set("view engine", "ejs")
 
-//Cerinta 6 etapa 4 folder special definit ca static
 
 
 // curs 5
-// Cerinta 13 etapa 4
+// Cerinta 13 etapa 4 si etapa 5 cerinta 2 a
 obGlobal={
     obErori:null,
     obImagini:null,
@@ -145,10 +165,85 @@ app.use(function(req, res, next) {
 });
 
 
-// Cerinta 8 etapa 4
-app.get(["/", "/index", "/home"], function(req,res){
-    res.render("pagini/index")
+// Etapa 5 Cerinta 1
+
+async function procesareImaginiGalerie() {
+    const caleJson = path.join(__dirname, "resurse", "json", "galerie.json");
+    if (!fs.existsSync(caleJson)) return [];
+    
+    const dateGalerie = JSON.parse(fs.readFileSync(caleJson, "utf8"));
+    
+    const minutCurent = new Date().getMinutes();
+    let sfert = 1;
+    if (minutCurent >= 15 && minutCurent < 30) sfert = 2;
+    else if (minutCurent >= 30 && minutCurent < 45) sfert = 3;
+    else if (minutCurent >= 45) sfert = 4;
+
+    let imaginiFiltrate = dateGalerie.imagini.filter(img => parseInt(img.sfert_ora) === sfert);
+    if (imaginiFiltrate.length > 10) {
+        imaginiFiltrate = imaginiFiltrate.slice(0, 10);
+    }
+
+    let caleFolder = path.join(__dirname, dateGalerie.cale_galerie);
+    for (let img of imaginiFiltrate) {
+        let caleAbsolutaOriginal = path.join(caleFolder, img.cale_imagine);
+        let extensie = path.extname(img.cale_imagine); // ex: .jpg
+        let numeFaraExt = path.basename(img.cale_imagine, extensie); // ex: poza1
+
+        let numeMic = numeFaraExt + "-mic" + extensie; // devine poza1-mic.jpg
+        let caleAbsolutaMic = path.join(caleFolder, numeMic);
+
+        img.cale_relativa = "/" + dateGalerie.cale_galerie + "/" + img.cale_imagine;
+        img.cale_mica_relativa = "/" + dateGalerie.cale_galerie + "/" + numeMic;
+
+        if (fs.existsSync(caleAbsolutaOriginal) && !fs.existsSync(caleAbsolutaMic)) {
+            await sharp(caleAbsolutaOriginal).resize(300).toFile(caleAbsolutaMic);
+        }
+    }
+    return imaginiFiltrate;
+}
+
+// Cerinta 8 etapa 4 si etapa 5 cerinta 1
+app.get(["/", "/index", "/home"], async function(req,res){
+    let imaginiGalerie = await procesareImaginiGalerie();
+    res.render("pagini/index", { ip: req.ip, imagini: imaginiGalerie });
+} )
+
+//etapa 6
+app.get("/produse", function(req,res){
+    client.query("select * from prajituri where id>3", function(err, rez){
+    if (err){
+        console.log("Eroare", err)
+        afisareEroare(res, 2)
+    }
+    else{
+        res.render("pagini/produse", { produse: rez.rows, optiuni:[]});
+    }
 })
+})
+
+app.get("/produs/:id", function(req,res){
+    clauzaWhere="";
+    if(req.query.tip){
+        clauzaWhere=`where tip_produs='${req.query.tip}'`
+    }
+    client.query(`select * from prajituri ${clauzaWhere}`, function(err, rez){
+    if (err){
+        console.log("Eroare", err)
+        afisareEroare(res, 2)
+    }
+    else{
+        if(rez.rowcount==0){
+            afisareEroare(res, 404, "Produs inexistent")
+        }
+        else{
+            res.render("pagini/produs", { produs: rez.rows[0] });
+        }
+    }
+})
+})
+
+//etapa 6
 
 app.get("/despre", function(req,res){
     res.render("pagini/despre")
@@ -179,7 +274,7 @@ app.get("/cale2", function(req,res)
 )
 
 // curs 5
-// Cerinta 13 etapa 4 - citire erori din fisier json si salvare in obGlobal
+// Cerinta 13 etapa 4 citire erori din fisier json si salvare in obGlobal
 function initErori(){
     // important de stiut fisire sincron si asincron, daca e sincron, codul se opreste pana se citeste fisierul, daca e asincron, codul continua sa ruleze si cand se termina de citit fisierul, se executa callback-ul
     let continut = fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8");
@@ -194,7 +289,7 @@ function initErori(){
 initErori()
 
 
-// Cerinta 14 etapa 4 - functie pentru afisare eroare, care primeste ca parametru res, identificatorul erorii, titlu, text, imagine
+// Cerinta 14 etapa 4 functie pentru afisare eroare
 function afisareEroare(res, identificator, titlu, text, imagine){
     let eroare=obGlobal.obErori.info_erori.find((elem)=>
         elem.identificator==identificator
@@ -215,9 +310,9 @@ app.get("/eroare", function(req, res){
     afisareEroare(res, 404, "Eroare 404 - Pagina nu a fost gasita");
 });
 
+// Etapa 5 cerinta 2 b
 function compileazaScss(caleScss, caleCss){
     if(!caleCss){
-
         let numeFisExt=path.basename(caleScss);
         let numeFis=numeFisExt.split(".")[0];
         caleCss=numeFis+".css";
@@ -228,6 +323,7 @@ function compileazaScss(caleScss, caleCss){
     if (!path.isAbsolute(caleCss))
         caleCss=path.join(obGlobal.folderCss,caleCss )
     
+    // Etapa 5 Cerinta 2 c
     let caleBackup=path.join(obGlobal.folderBackup, "resurse/css");
     if (!fs.existsSync(caleBackup)) {
         fs.mkdirSync(caleBackup,{recursive:true})
@@ -235,14 +331,25 @@ function compileazaScss(caleScss, caleCss){
 
     let numeFisCss=path.basename(caleCss);
     if (fs.existsSync(caleCss)){
-        fs.copyFileSync(caleCss, path.join(obGlobal.folderBackup, "resurse/css",numeFisCss ))// +(new Date()).getTime()
+        //Etapa 5 cerinta 2 c - daca exista fisierul css, fac backup in folderul backup cu acelasi nume + timestamp
+        try {
+            // Etapa 5 Cerinta 2 d si e
+            let numeBackup = numeFisCss.replace('.css', '') + "_" + new Date().getTime() + ".css";
+            fs.copyFileSync(caleCss, path.join(caleBackup, numeBackup));
+        } catch (err) {
+            console.error("Eroare la crearea backup-ului pentru " + numeFisCss + ": ", err.message);
+        }
     }
-    rez=sass.compile(caleScss, {"sourceMap":true});
-    fs.writeFileSync(caleCss,rez.css)
-    
+    // Etapa 5 cerinta 2 b
+    try {
+        let rez=sass.compile(caleScss, {"sourceMap":true});
+        fs.writeFileSync(caleCss,rez.css);
+    } catch (err) {
+        console.error("Eroare SASS la fișierul " + caleScss + ": ", err.message);
+    }
 }
 
-
+// Etapa 5 Cerinta 2 d
 // la pornirea serverului, compilez toate fisierele scss din folderul resurse/scss
 vFisiere=fs.readdirSync(obGlobal.folderScss);
 for( let numeFis of vFisiere ){
@@ -251,7 +358,7 @@ for( let numeFis of vFisiere ){
     }
 }
 
-
+// Etapa 5 Cerinta 2 e
 fs.watch(obGlobal.folderScss, function(eveniment, numeFis){
     if (eveniment=="change" || eveniment=="rename"){
         let caleCompleta=path.join(obGlobal.folderScss, numeFis);
